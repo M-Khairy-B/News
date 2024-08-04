d2f4aa70be762f0a30c6bd951e6653d5b19a5a91
Mohamed Khairy <Mohamedbakr225@gmail.com>
2024-08-04 03:37:02 +0300

    Done

--- src/component/Science/Science.jsx
+++ src/component/Science/Science.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ScienceSlider from '../ScienceSlider/ScienceSlider'
 import ScienceArticals from '../ScienceArticals/ScienceArticals'
+import { Helmet } from "react-helmet";
 
 export default function Science() {
   return (<React.Fragment>
-
+         <Helmet>
+                <title> Science</title>
+                <meta name="description" content="Technology " />
+            </Helmet>
     <ScienceSlider/>
     <ScienceArticals/>
   </React.Fragment>
